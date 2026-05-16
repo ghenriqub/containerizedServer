12b672ceed70db635851c3d4e2f0ce91fa1214ce
ghenriqub <ghenriqu@student.42porto.com>
2026-05-16 17:55:15 +0100

    Update

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -6,16 +6,17 @@
 #    By: ghenriqu <ghenriqu@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/05/10 18:49:10 by ghenriqu          #+#    #+#              #
-#    Updated: 2026/05/10 19:05:55 by ghenriqu         ###   ########.fr        #
+#    Updated: 2026/05/16 17:21:11 by ghenriqu         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 set -e
 
 envsubst '$DOMAIN_NAME' < /etc/nginx/nginx.conf > /tmp/nginx.conf
-mv /tmp/nginx.conf /etc/nginx/nginx.conf
-echo "Resolved DOMAIN_NAME=${DOMAIN_NAME} in nginx.conf"
 
+mv /tmp/nginx.conf /etc/nginx/nginx.conf
+
+echo "Resolved DOMAIN_NAME=${DOMAIN_NAME} in nginx.conf"
 
 if [ ! -f /etc/nginx/ssl/server.crt ] || [ ! -f /etc/nginx/ssl/server.key ]; then
     echo "Error: TLS certificate or key not found at /etc/nginx/ssl/"
