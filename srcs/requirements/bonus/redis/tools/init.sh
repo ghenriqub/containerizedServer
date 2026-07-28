a502f8be817d41af9a4ffa3fae1c24bc216e3863
Gabriel Barbosa <ghrb2811@gmail.com>
2026-07-28 16:28:04 +0100

    Bonus: redis ok

---
+++ srcs/requirements/bonus/redis/tools/init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    init.sh                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ghenriqu <ghenriqu@student.42porto.com>    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/07/28 00:00:00 by ghenriqu          #+#    #+#              #
+#    Updated: 2026/07/28 00:00:00 by ghenriqu         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+set -eu
+
+CONF_TEMPLATE="/etc/redis/redis.conf"
+RUNTIME_CONF="/run/redis/redis.conf"
+SECRET_FILE="/run/secrets/redis_password"
+
+if [ ! -s "${SECRET_FILE}" ]; then
+    echo "ERROR: ${SECRET_FILE} missing or empty." >&2
+    exit 1
+fi
+
+mkdir -p /run/redis
+cp "${CONF_TEMPLATE}" "${RUNTIME_CONF}"
+printf 'requirepass %s\n' "$(cat "${SECRET_FILE}")" >> "${RUNTIME_CONF}"
+
+chown -R redis:redis /run/redis /var/lib/redis
+chmod 700 /run/redis
+chmod 600 "${RUNTIME_CONF}"
+
+echo "[redis] starting redis-server as PID 1 (user: redis)..."
+
+exec setpriv --reuid=redis --regid=redis --init-groups \
+     redis-server "${RUNTIME_CONF}"
